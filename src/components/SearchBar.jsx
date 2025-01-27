e33f73dbb5aeb5e549281b23c2cab83ea1c9b0cb
shamshersarpal <shamshersingh01567@gmail.com>
2025-01-27 15:59:15 +0530

    update

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,8 @@ import { DatePickerWithRange } from "./DatePicker";
 export default function SearchBar() {
   return (
     <>
-      <section className="py-2 bg-white   mx-auto rounded-lg shadow-lg max-w-[950px] mt-2">
+    <div className="mx-4">
+    <section className="py-2 bg-white   mx-auto rounded-lg shadow-lg max-w-[955px] mt-2">
         <div className="container mx-auto px-3 ">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 ">
             {/* Column 1 */}
@@ -130,7 +131,7 @@ export default function SearchBar() {
                 <label
                   htmlFor="countries"
                   className="block mb-2 text-sm font-medium text-white  "
-                >cc
+                > s
 
                 </label>
                 <button type="button" className="text-white justify-center bg-gradient-to-r w-full flex items-center gap-1 from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
@@ -144,6 +145,8 @@ export default function SearchBar() {
           </div>
         </div>
       </section>
+    </div>
+      
     </>
   );
 }
