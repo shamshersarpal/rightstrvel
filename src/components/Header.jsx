a9240ca4065324848656fd84d0d69e400f2b2fba
shamshersarpal <shamshersingh01567@gmail.com>
2025-01-29 10:25:23 +0530

    uploade

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -142,7 +142,7 @@ export default function Header() {
             >
               <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
                 <li>
-                  <Link href="/" passHref legacyBehavior>
+                  <Link href="/login" passHref legacyBehavior>
                     <a
 
                       className={`block py-2 px-3 rounded-sm md:p-0 uppercase ${activeItem === 'myAccount' ? 'text-white bg-blue-700 md:bg-transparent md:text-blue-700' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700'}`}
